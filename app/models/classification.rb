6059f2a9cb23431cbc572049421361a7356919ed
Simon Tokumine <simon@japancentre.com>
2010-05-27 18:06:14 +0200

    added basic game

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,14 +2,15 @@ class Classification < ActiveRecord::Base
   belongs_to :track
   belongs_to :cell
   attr_accessible :x, :y, :z, :value, :track, :cell
+  acts_as_list :scope => :track
   after_update :update_stats
-  
+    
   def validate_on_update
     if changes["value"].first != nil
-      errors.add (:value, "can't reclassify an existing classification")
+      errors.add(:value, "can't reclassify an existing classification")
     end  
   end
-  
+    
   def update_stats
     self.track.user.refresh_stats
     self.cell.update_totals :positive => value
