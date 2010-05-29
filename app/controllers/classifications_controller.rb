9c7588ccd43d2a35db1eb6054f1fbad9d6f94f77
Simon Tokumine <simon@japancentre.com>
2010-05-29 21:57:49 +0200

    uppacked gems

--- app/controllers/classifications_controller.rb
+++ app/controllers/classifications_controller.rb
@@ -2,7 +2,7 @@ class ClassificationsController < ApplicationController
   before_filter :require_user, :only => [:index] 
   
   def update
-    @classification = Classification.find(params[:id], :include => {:track => :user, :cell})    
+    @classification = Classification.find(params[:id], :include => [{:track => :user}, :cell])    
     
     if @classification.update_attributes(:value => params[:value]) && current_user.tracks.include?(@classification.track)
       render :json => {:update => true}, :callback => params[:callback]  
