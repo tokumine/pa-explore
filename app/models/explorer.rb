5c9e5915077d351db10e46446a3822fe6eaaf71e
Simon Tokumine <simon@japancentre.com>
2010-05-29 22:14:21 +0200

    uppacked gems

--- app/models/explorer.rb
+++ app/models/explorer.rb
@@ -44,22 +44,22 @@ class Explorer
   #
   def move
       
-    cells = Cell.all :conditions => "(x = #{@x-1} AND y = #{@y}) OR (x = #{@x+1} AND y = #{@y}) OR (x = #{@x} AND y = #{@y-1}) OR (x = #{@x} AND y = #{@y+1})",
-                     :order => "positive_count + negative_count ASC"
+    #cells = Cell.all :conditions => "(x = #{@x-1} AND y = #{@y}) OR (x = #{@x+1} AND y = #{@y}) OR (x = #{@x} AND y = #{@y-1}) OR (x = #{@x} AND y = #{@y+1})",
+    #                 :order => "positive_count + negative_count ASC"
     
     # if all the cells have already been surveyed, weight index to those with few surveys                 
-    if cells.size == 4
-      i = rand(3)
-      i = (i - (i * 0.1)).floor      
-      @x = cells[i].x
-      @y = cells[i].y
-      return
-    end
+    #if cells.size == 4
+    #  i = rand(3)
+    #  i = (i - (i * 0.1)).floor      
+    #  @x = cells[i].x
+    #  @y = cells[i].y
+    #  return
+    #end
     
     # if there are empty cells, make a new cell where there's a gap and use that 
-    possible_cells = [[@x+1, @y],[@x-1, @y],[@x, @y-1]] #[@x, @y+1] *<-- ALWAYS GOING DOWN
-    existing_cells = cells.map {|c| [c.x, c.y]}
-    survey_cell = (possible_cells - existing_cells).rand
+    possible_cells = [[@x+1, @y],[@x-1, @y],[@x+1, @y],[@x-1, @y],[@x, @y-1]].rand #[@x, @y+1] *<-- ALWAYS GOING DOWN
+    #existing_cells = cells.map {|c| [c.x, c.y]}
+    #survey_cell = (possible_cells - existing_cells).rand
     @x = survey_cell[0]
     @y = survey_cell[1]
   end
