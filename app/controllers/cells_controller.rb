1d2ddcf002529985ce3c9949fa2beb7812b5f5b8
Simon Tokumine <simon@japancentre.com>
2010-05-30 17:59:31 +0200

    changed to pure sql finder

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -1,6 +1,6 @@
 class CellsController < ApplicationController
   def tiles
-    cells = Cell.find_all_by_parent_x_and_parent_y_and_parent_z(params[:x],params[:y],params[:z])    
+    cells = Cell.all :conditions => "parent_x = #{params[:x]} AND parent_y = #{params[:y]} AND parent_z = #{params[:z]}"
     json = cells.inject([]) { |a,c| a << c.game_json }
     render :json => json, :callback => params[:callback]    
   end  
