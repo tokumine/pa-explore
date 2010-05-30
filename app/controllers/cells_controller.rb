eddd57e08dc479b69733e6c469e818cfa10f9b48
Simon Tokumine <simon@japancentre.com>
2010-05-30 19:26:07 +0200

    added memcached

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -1,6 +1,6 @@
 class CellsController < ApplicationController
   def tiles
-    cells = Cell.all :conditions => "parent_x = #{params[:x]} AND parent_y = #{params[:y]} AND parent_z = #{params[:z]}"
+    cells = Cell.all :conditions => {:parent_x => params[:x], :parent_y => params[:y], :parent_z => params[:z]}
     json = cells.inject([]) { |a,c| a << c.game_json }
     render :json => json, :callback => params[:callback]    
   end  
