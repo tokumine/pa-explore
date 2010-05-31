29e6bbceb9db79afae55282c004c6bc861f3e51f
Simon Tokumine <simon@japancentre.com>
2010-05-31 19:28:44 +0200

    restricted reanges

--- app/models/track.rb
+++ app/models/track.rb
@@ -17,8 +17,8 @@ class Track < ActiveRecord::Base
   def generate_track (x=nil, y=nil)    
 
     # Can set starting location by passing {:x => x, :y => y, :z => z} hash
-    x = (63520..63700).to_a.rand
-    y = (51220..51400).to_a.rand
+    x = (63520..63600).to_a.rand
+    y = (51220..51300).to_a.rand
     #x = 63520
     #y = 51220
     
